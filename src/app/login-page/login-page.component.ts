7c5f06273facaf6673fe21684abba92119cf7cdd
Naveeduddin Mohammed <mnaveeduddin@keypoint-tech.com>
2022-08-12 17:42:32 +0530

    connected api and storing loginData in db

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { AppService } from '../app.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -21,16 +22,27 @@ export class LoginPageComponent implements OnInit {
     password: new FormControl('')
   });
 
-  username:string = '';
+ 
 
-  constructor(private router: Router, private appservice: AppService) { }
+  usernameData:string = '';
+  passwordData:string = '';
+
+  constructor(private router: Router, private appservice: AppService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   transfer(){
-    this.appservice.setValue(this.loginForm.value);
-    this.appservice.setInfo(this.username);
+    // this.appservice.setValue(this.loginForm.value);
+     let loginData = {
+    usernameData: this.usernameData,
+    passwordData: this.passwordData
+  }
+    this.appservice.setInfo(this.usernameData);
+
+    this.http.post('http://localhost:8080/login', loginData).subscribe((res) => {
+      console.log(res);
+    })
     this.router.navigate(['/components'])
   }
 }
